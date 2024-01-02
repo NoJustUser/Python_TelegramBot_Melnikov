83a40054d58014d6bf3a92647b232e95bba610bd
NoJustUser <melnikov.alex.andr@yandex.ru>
2024-01-02 23:28:26 +0300

    ask_move and make_move were been changed

--- ask_and_make_move.py
+++ ask_and_make_move.py
@@ -1,22 +1,19 @@
-def ask_move(player):
+def ask_move(player, board):
     # дать игроку возможность сделать ход, то есть  ввести координаты
     x, y = input(f"{player}, Введите координаты ячейки (напр. 0 0): ").strip().split()
-    # преобразовать координаты в целые числа
     x, y = int(x), int(y)
-    return x, y
-
-
-def make_move(x, y, player, board):
     # задать условие, которое проверяет,
     # находится ли координата в пределах поля и свободно ли место
     if (0 <= x <= 2) and (0 <= y <= 2) and (board[x][y] == " "):
-        # если свободно, записать значение игрока (Х или 0) в ячейку
-        board[x][y] = player
-    else:
-        print("Клетка занята, попробуйте снова.")
-        ask_move(player)
+        return x, y
+    print('Клетка занята. Введите координаты еще раз.')
+    ask_move(player, board)
+
+
+def make_move(x, y, player, board):
+    board[x][y] = player
 
 
 def ask_and_make_move(player, board):
-    x, y = ask_move(player)
+    x, y = ask_move(player, board)
     make_move(x, y, player, board)
