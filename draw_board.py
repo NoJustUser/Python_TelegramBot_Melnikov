fbf045e8927bf64bff71adab352793bfd4770312
NoJustUser <melnikov.alex.andr@yandex.ru>
2024-01-06 23:45:49 +0300

    draw_board was modified

--- draw_board.py
+++ draw_board.py
@@ -1,3 +1,18 @@
+from colorama import Fore, Back, Style, init
+
+init()
+
+colors = {
+    'X': Fore.RED + 'X' + Style.RESET_ALL,
+    '0': Fore.BLUE + 'O' + Style.RESET_ALL,
+    ' ': Back.YELLOW + ' ' + Style.RESET_ALL,
+}
+
+
 def draw_board(board):
-    res = [' | '.join(v) for v in board]
-    print(*res, sep='\n---------\n')
+    res = []
+    for i in range(3):
+        tmp = [colors.get(v) for v in board[i]]
+        res.append(tmp)
+    prnt = [' | '.join(v) for v in res]
+    print(*prnt, sep='\n---------\n')
