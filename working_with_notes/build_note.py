feb1485d31080b24b97ff00db30cbfd16e95e60b
NoJustUser <melnikov.alex.andr@yandex.ru>
2024-01-13 16:02:27 +0300

    New folders and files were created

---
+++ working_with_notes/build_note.py
@@ -0,0 +1,5 @@
+def build_note(note_text, note_name):
+    with open(f"{note_name}.txt", "w", encoding="utf-8") as file:
+        file.write(note_text)
+    print(f"Заметка {note_name} создана.")
+
---
+++ working_with_notes/create_note.py
@@ -0,0 +1,8 @@
+from build_note import build_note
+
+
+def create_note():
+    note_name = input('Введите название заметки :')
+    note_text = input('Введите текст заметки :')
+    build_note(note_text, note_name)
+
