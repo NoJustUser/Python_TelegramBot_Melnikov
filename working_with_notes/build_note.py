6cc5fdf037e0491cb147acd818835a749970b20d
NoJustUser <melnikov.alex.andr@yandex.ru>
2024-01-13 16:24:44 +0300

    read_note.py was added

--- working_with_notes/build_note.py
+++ working_with_notes/build_note.py
@@ -1,5 +1,5 @@
 def build_note(note_text, note_name):
-    with open(f"{note_name}.txt", "w", encoding="utf-8") as file:
+    with open(f'{note_name}.txt', 'w', encoding='utf-8') as file:
         file.write(note_text)
-    print(f"Заметка {note_name} создана.")
+    print(f'Заметка {note_name} создана.')
 
--- working_with_notes/create_note.py
+++ working_with_notes/create_note.py
@@ -2,7 +2,7 @@ from build_note import build_note
 
 
 def create_note():
-    note_name = input('Введите название заметки :')
-    note_text = input('Введите текст заметки :')
+    note_name = input('Введите название заметки : ')
+    note_text = input('Введите текст заметки : ')
     build_note(note_text, note_name)
 
