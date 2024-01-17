5ea10142a93ebd23d621b47e9471ad89ed7dd351
NoJustUser <melnikov.alex.andr@yandex.ru>
2024-01-17 22:49:21 +0300

    display_notes gives sorted notes

--- working_with_notes/display_notes.py
+++ working_with_notes/display_notes.py
@@ -3,11 +3,15 @@ import os.path
 
 def display_notes():
     files = os.listdir()
+    len_dict = {}
     list_notes = [file for file in files if file.endswith('.txt')]
+
     for note in list_notes:
         with open(note, 'r') as file:
             text = file.read()
+        len_dict[text] = len(text)
+
+    res = sorted(len_dict.items(), key=lambda x: x[1])
+    for text, len_text in res:
         print(text)
 
-
-display_notes()
